376300018089bb1bfde37ebf517e288bbbb2df63
Saulo Belchior <saulo.belchior@gmail.com>
2024-07-08 16:55:40 -0300

    Delete front directory

--- front/src/api/get-history.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "@/lib/axios"
-
-const getHistory = async () => {
-  return await api.get('/v1/file/history')
-}
-
-export { getHistory }
--- front/src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './layout';
-export * from './no-match';
-export * from './file-uploader';
-export * from './table';
-export * from './skeleton';
-export * from './scroll-area';
