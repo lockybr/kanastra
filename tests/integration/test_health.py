d6ccac4842fbd3076f2c1f4adce4251326225b7f
Saulo Belchior <saulo.belchior@gmail.com>
2024-07-08 16:56:02 -0300

    Delete tests directory

--- tests/integration/test_health.py
+++
@@ -1,6 +0,0 @@
-def test_health_should_return_200(client):
-  response = client.get("/health")
-  assert response.status_code == 200
-  assert response.json() == {
-    "status": "OK"
-  }
--- tests/unit/domain/repositories/test_bank_slip_repository.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from src.domain.repositories.bank_slip_repository import BankSlipRepositoryProtocol
-
-def test_generate_should_raise_not_implemented_error(register_model):
-  with pytest.raises(NotImplementedError):
-    BankSlipRepositoryProtocol.generate(register_model)
